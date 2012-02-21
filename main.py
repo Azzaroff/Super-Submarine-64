2acd536f8703e12143b65412e507f496cb2b96f1
Azzaroff <azzaroff@felux.fritz.box>
2012-02-21 17:53:34 +0100

    WTF

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ class Application:
         _mat = avango.osg.make_scale_mat(100000,100000,100000) * avango.osg.make_rot_mat(math.radians(180),1,0,0) * avango.osg.make_rot_mat(math.radians(0),0,1,0)
         self.skybox = avango.osg.nodes.LoadFile(Filename = "data/skybox.obj", Matrix = _mat)
         self.Scene.environment_root.Children.value.append(self.skybox)
-        
+
         self.Scene.navigation_transform.Matrix.value = avango.osg.make_trans_mat(0.0,0.0,0.0)
 
 
